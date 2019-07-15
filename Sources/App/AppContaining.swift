f5f0f51df9a6159a7551dd8b5178a0c3be50e57b
Andrea Altea <oni.zerone@gmail.com>
2019-07-15 17:32:08 +0200

    Feat: update template with saber and cocoapods

---
+++ Sources/App/AppContaining.swift
@@ -0,0 +1,15 @@
+//
+//  AppContaining.swift
+//  Skeleton
+//
+//  Created by Andrea Altea on 15/07/2019.
+//  Copyright © 2019 Andrea Altea. All rights reserved.
+//
+
+import Foundation
+
+typealias Lazy<T> = () -> T
+
+// @saber.container(AppContainer)
+// @saber.scope(App)
+protocol AppContaining {}
--- Sources/App/AppDelegate.swift
+++ Sources/App/AppDelegate.swift
@@ -12,10 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    var appContainer = AppContainer()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = UIViewController()
+        self.window?.makeKeyAndVisible()
         return true
     }
 
